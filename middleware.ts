e545888e3624714a59e59d7f5560c562fd27cc86
Joey Zhao <zhaoyi0113@gmail.com>
2024-08-22 10:52:31 +1000

    test

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
 
 // Set pathname were middleware will be executed
 export const config = {
   matcher: ['/edge', '/api/hello'],
-}
+};
 
 export function middleware(req: NextRequest) {
   // Get country
   console.log('request country:', req.geo);
-  const country = req.geo?.country?.toLowerCase() || 'au'
+  const country = req.geo?.country?.toLowerCase() || 'au';
 
   console.log('middleware before: ', req.nextUrl.pathname);
   // Update pathname
-  req.nextUrl.pathname += `/${country}`
+  req.nextUrl.pathname += `/${country}`;
   console.log('middleware after', req.nextUrl.pathname);
 
   // Rewrite to URL
-  return NextResponse.rewrite(req.nextUrl)
+  return NextResponse.rewrite(req.nextUrl);
 }
